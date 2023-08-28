99afae5b48fa4c556313b150114a73ccb5fe673e
Authun Ayyan <107317347+AuthunC@users.noreply.github.com>
2023-08-28 23:48:40 +0530

    Update empreg.js

--- Emp Reg/empreg.js
+++ Emp Reg/empreg.js
@@ -1,10 +1,3 @@
-// const y=document.forms["form"];
-// let text=" "
-// for(let i=0;i<y.length;i++){
-//     text =  text + y.elements[i].value + "<br>";
-// }
-// document.getElementById("demo").innerHTML=text;
-
 function validateForm(){
     let x=document.forms["forms"]["fnames"].value;
     if (x == ""){
